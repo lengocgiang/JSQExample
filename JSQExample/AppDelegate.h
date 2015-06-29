aedc7943da5abeb63b1884197b0686bcd6d7586b
Giang Le Ngoc <giangln@seesaa.vn>
2015-06-29 14:55:58 +0700

    Initial Commit

---
+++ JSQExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSQExample
+//
+//  Created by Le Ngoc Giang on 6/29/15.
+//  Copyright (c) 2015 giangle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
